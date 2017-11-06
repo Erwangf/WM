780463b78af92efc851e6705e793059ff61a3ec5
erwangf <erwangf@gmail.com>
2017-11-06 09:04:10 +0100

    bugfix cluster + calcul nb edges, vertices

--- src/main/scala/run/local/SparkLinkParser.scala
+++ src/main/scala/run/local/SparkLinkParser.scala
@@ -4,12 +4,14 @@ import run.shared.LinkParser
 import org.apache.spark.sql.{Row, SparkSession}
 import scala.collection.mutable
 import scala.util.matching.Regex
+
 object SparkLinkParser {
 
   def main(args: Array[String]): Unit = {
 
 
     var filePath = this.getClass.getClassLoader.getResource("xml.txt").getPath
+    filePath = "D:\\Users\\Erwan\\Documents\\M2\\Big Data Management\\projet\\frwiki-20171001-pages-articles-multistream.xml"
 
     var masterInfo = "local[*]"
     val ss = SparkSession.builder().appName("LinkParser").master(masterInfo).getOrCreate()
@@ -42,18 +44,27 @@ object SparkLinkParser {
 
     df = df.withColumn("edges", udfParser(df.col("title"), df.col("text")))
 
-    println("Loaded "+df.count()+" pages !")
+
 
     // Now we will extract all the edges
     // first we take 1000 edges, then convert Array[WrappedArray[Row]] to Array[WrappedArray[(String,String)]]
     // That's because when importing a Tuple (String,String), Spark convert it to a struct, which is basically a Row.
     // So we got Rows (our edges) containings Rows (origin,destination)... :p
     df.select("edges")
-      .take(1000)
+      .take(10)
       .map(_.get(0)
         .asInstanceOf[mutable.WrappedArray[Row]]
         .map(r => (r.get(0).asInstanceOf[String], r.get(1).asInstanceOf[String]))
       ) // then we print all this edges
       .foreach(e => e.foreach(i => println(i._1 + ";" + i._2)))
+
+    // Calculate number of vertices and edges with a Reduce
+    // we need to import sparkSession.implicits._ in order to map Row into Tuple
+    import ss.implicits._
+    val counts = df
+      .map(r => (1, r.get(r.fieldIndex("edges")).asInstanceOf[mutable.WrappedArray[Row]].length))
+      .reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+    println(s"Nb Vertices : ${counts._1}\nNb Edges : ${counts._2}")
+
   }
 }
--- src/main/scala/run/shared/LinkParser.scala
+++ src/main/scala/run/shared/LinkParser.scala
@@ -4,24 +4,26 @@ import scala.util.matching.Regex
 import scala.xml.XML
 import java.io._
 
-object LinkParser{
-    /**THis is a public function to be used, no object creation needed
+object LinkParser {
+  /** THis is a public function to be used, no object creation needed
     * take a title and a row string to create an Array of vertices
     *
     * @param title the page title (meaning the starting edge)
     * @param bob   the raw text to parse in order to extract references to other pages
     * @return Array[(String,String)] of start_edge(title) -> end_edge(referenced page)
     */
- def externalParser(title: String, bob: String): Array[(String, String)] = {
+  def externalParser(title: String, bob: String): Array[(String, String)] = {
     val keyValPattern: Regex = "\\[\\[(.+?)\\]\\]".r
     var out_final: Array[(String, String)] = Array()
-    for (e <- bob.split('\n')) {
-      for (patternMatch <- keyValPattern.findAllMatchIn(e)) {
-        val link = patternMatch.group(1)
-        if (!link.contains(":")) {
-          // Sometimes, link.split("#")(0).split("\\|") is empty causing an ArrayIndexOutOfBoundsException
-          if(link.split("#").length!=0 && link.split("#")(0).split("\\|").length!=0){
-            out_final = out_final :+ (title, link.split("#")(0).split("\\|")(0))
+    if (bob != null && title != null) {
+      for (e <- bob.split('\n')) {
+        for (patternMatch <- keyValPattern.findAllMatchIn(e)) {
+          val link = patternMatch.group(1)
+          if (!link.contains(":")) {
+            // Sometimes, link.split("#")(0).split("\\|") is empty causing an ArrayIndexOutOfBoundsException
+            if (link.split("#").length != 0 && link.split("#")(0).split("\\|").length != 0) {
+              out_final = out_final :+ (title, link.split("#")(0).split("\\|")(0))
+            }
           }
         }
       }
