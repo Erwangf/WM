5b1eb9c979e1b5924393ccfb93c76c2300814a98
erwangf <erwangf@gmail.com>
2017-11-06 12:23:40 +0100

    Avancement AppParams

--- src/main/java/webapp/config/AppParams.java
+++ src/main/java/webapp/config/AppParams.java
@@ -1,8 +1,24 @@
 package webapp.config;
 
 public class AppParams {
-    private static String appName;
-    private static String sparkMaster;
+
+
+    private String sparkMaster;
+    private int numExecutors;
+    private String sparkExecutorsMemory;
+    private int sparkExecutorCores;
+
+
+    private AppParams() {
+        // TODO : load config from file ?
+    }
+
+    // loading app params on JVM startup
+    private static AppParams INSTANCE = new AppParams();
+
+    public static AppParams getInstance() {
+        return INSTANCE;
+    }
 
 
 }
